f84729d1711f98bca3492e89fca57ac3421ff3bf
Adhiraj Singh <adhirajsingh1001@gmail.com>
2024-03-25 20:34:03 +0530

    feat: self crypto support

--- src/utils/webcrypto.ts
+++ src/utils/webcrypto.ts
@@ -1,12 +1,14 @@
-import type { webcrypto as Crypto } from 'crypto'
-
 export const webcrypto = (() => {
 	// if we're in node, we need to use
 	// webcrypto provided by the crypto module
-	if(typeof window === 'undefined') {
-		const { webcrypto } = require('crypto')
-		return webcrypto as Crypto
+	if(typeof window !== 'undefined') {
+		return window.crypto
 	}
 
-	return window.crypto
+	if(self.crypto !== undefined) {
+		return self.crypto
+	}
+
+	const { webcrypto } = require('crypto')
+	return webcrypto as Crypto
 })()
